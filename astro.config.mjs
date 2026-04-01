da1a5a902b24ac2f558fce1b306c2357437a0e16
dongyeul <rlaehdduf04@gmail.com>
2026-04-01 12:19:42 +0900

    config: remove base for user repository deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rlaehdduf.github.io',
-  base: '/portfolio',
   output: 'static',
 });
